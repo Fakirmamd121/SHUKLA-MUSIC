fcca6fdb2b877e3e83f3c2aabe416be997bc3fd2
Anime lover <botm70554@gmail.com>
2024-05-22 22:11:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -141,10 +141,10 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/7063e3b0eac98dcc5734e.jpg"
+    "START_IMG_URL", "https://graph.org/file/4e9c829771bd1e680c62a.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/35f71c3ebde68a5d7fc23.jpg"
+    "PING_IMG_URL", "https://graph.org/file/4e9c829771bd1e680c62a.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/181715eb83d6b2dc4c1a1.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/3e8be4617f67d7369b44a.jpg"
